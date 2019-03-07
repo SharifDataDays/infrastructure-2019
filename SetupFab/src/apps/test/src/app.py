380db92d66b0a82a0f01019d81b028c65a7b5f3a
vahidzee <vahid98zee@gmail.com>
2019-03-07 04:27:04 +0330

    semi final setup on test

--- SetupFab/src/apps/test/src/app.py
+++ SetupFab/src/apps/test/src/app.py
@@ -10,14 +10,16 @@ def action(connection_tuple: tuple):
         adduser --disabled-password --force-badname --gecos "" datadays
         su datadays
         pip3 install --ignore-installed tornado==4.5 --user
-        nohup jupyter notebook --ip=0.0.0.0 --port=8321 --NotebookApp.token= --NotebookApp.password= 0<&- &> my.admin.log.file &
+        pip3 install tensorflow --user
+        screen -S jup
+        jupyter notebook --ip=0.0.0.0 --port=8321 --NotebookApp.token= --NotebookApp.password=
+        screen -d jup
         exit
-        echo "datadays:1234" | chpasswd
         """
     )
+
     if results['ok']:
-        if 'Created TensorFlow device' in results['tensor_test']:
-            return str(connection_tuple[1]) + ' ' + str(connection_tuple[2]) + ' ok'
-        return str(connection_tuple[1]) + ' ' + str(connection_tuple[2]) + ' failed'
+        return str(connection_tuple[1]) + ' ' + str(connection_tuple[2]) + ' ok'
+        # return str(connection_tuple[1]) + ' ' + str(connection_tuple[2]) + ' failed'
     return str(connection_tuple[1]) + ' ' + str(connection_tuple[2]) + ' script error on : ' + str(
         results['failed_command'])
